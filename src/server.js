bc9db65455b2d4cd5e2cae5de4a17e8cacddda5c
Jun Hee Lee <junheelee@Junui-iMac.local>
2024-10-13 15:06:43 +0900

    setting socket.io on server-side

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const http = require("http");
+const server = http.Server(app);
+
+const socket = require("socket.io");
+const io = socket(server);
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
@@ -9,7 +15,11 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(__dirname + "/public");
   console.log("hello");
+});
+
+io.on("connetion", () => {
+  console.log("someone join");
 });
