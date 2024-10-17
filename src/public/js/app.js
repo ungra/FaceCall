5682a3daafed6849cc3438a6b99b7dcf10ff2862
junhee_lee2 <junhee_lee2@tmax.co.kr>
2024-10-17 22:16:52 +0900

    refactoring...

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,30 +1,26 @@
-const landingPage = document.getElementById("landingPageDiv");
-const callPage = document.getElementById("callPageDiv");
-const submitNickName = document.getElementById("submitNicknameDiv");
-const submitedNickName = document.getElementById("submittedUserNickNameDiv");
-const nickNameForm = document.getElementById("nickNameForm");
-const roomNameForm = document.getElementById("roomNameForm");
-const roomListDiv = document.getElementById("roomListDiv");
-const myVideo = document.getElementById("myCamera");
-const peerVideo = document.getElementById("peerCamera");
-const myVideoOffBt = document.getElementById("myVideoOff");
-const myAudioMuteBt = document.getElementById("myAudioMute");
-const myCameraSelect = document.getElementById("myVideo");
-const myAudioSelect = document.getElementById("myAudio");
+const landingPageDiv = document.getElementById("landingPage");
+const callPageDiv = document.getElementById("callPage");
+const beforeNNDiv = document.getElementById("beforeNickname");
+const afterNNDiv = document.getElementById("afterNickname");
+const nicknameForm = document.getElementById("nickname");
+const roomNameForm = document.getElementById("roomName");
+const roomListh3 = document.getElementById("roomList");
+const myCameraVideo = document.getElementById("myCamera");
+const peerCmeraVideo = document.getElementById("peerCamera");
+const myCameraOffBtn = document.getElementById("myCameraOff");
+const myAudioMuteBtn = document.getElementById("myAudioMute");
+const myCameraSelect = document.getElementById("myCameraList");
+const myAudioSelect = document.getElementById("myAudioList");
 
 let myNickname = "anonymous";
 let roomName;
 let myMediaStream;
-let myCamera = true;
-let myAudio = true;
-let myCameraList;
-let myAudioList;
 let myDevices;
 
-landingPage.hidden = false;
-callPage.hidden = true;
-submitNickName.hidden = false;
-submitedNickName.hidden = true;
+landingPageDiv.hidden = false;
+callPageDiv.hidden = true;
+beforeNNDiv.hidden = false;
+afterNNDiv.hidden = true;
 
 //socket io
 const socket = io();
@@ -37,10 +33,50 @@ socket.on("disconnect", () => {
   console.log("disconnect!!!");
 });
 
+//landing page
+function handleNickname(event) {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  console.log(`submit Nick Name: ${input.value}`);
+  myNickname = input.value;
+  beforeNNDiv.hidden = true;
+  afterNNDiv.hidden = false;
+  const h3 = document.createElement("h3");
+  h3.innerText = `User Nickname : ${myNickname}`;
+  afterNNDiv.appendChild(h3);
+  input.value = "";
+}
+function handleRoomName(event) {
+  event.preventDefault();
+  const input = roomNameForm.querySelector("input");
+  console.log(`submit Room Name: ${input.value}`);
+  roomName = input.value;
+  const li = document.createElement("li");
+  li.innerText = roomName;
+  roomListh3.appendChild(li);
+  input.value = "";
+  landingPageDiv.hidden = true;
+  callPageDiv.hidden = false;
+  initCallPage();
+}
+function initCallPage() {
+  const roomNameh2 = document.createElement("h2");
+  roomNameh2.innerText = `RoomName : ${roomName}`;
+  callPage.prepend(roomNameh2);
+  nicknameh2 = document.createElement("h2");
+  nicknameh2.innerText = `My NickName : ${myNickname}`;
+  callPage.prepend(nicknameh2);
+  settingMyMediaStream();
+}
+
+nicknameForm.addEventListener("submit", handleNickname);
+roomNameForm.addEventListener("submit", handleRoomName);
+
 //callPage
 
-function settingCameraList(myDevices) {
+function settingCameraList() {
   myCameraList = myDevices.filter((device) => device.kind === "videoinput");
+  console.log(myMediaStream.getVideoTracks().filter((track) => track.label));
   myCameraList.forEach((camera) => {
     const option = document.createElement("option");
     option.innerText = camera.label;
@@ -48,7 +84,7 @@ function settingCameraList(myDevices) {
   });
 }
 
-function setttingAudioList(myDevices) {
+function setttingAudioList() {
   myAudioList = myDevices.filter((device) => device.kind === "audioinput");
   myAudioList.forEach((audio) => {
     const option = document.createElement("option");
@@ -57,87 +93,68 @@ function setttingAudioList(myDevices) {
   });
 }
 
-async function getMyCamera() {
+async function settingMyMediaStream(cameraId, audioId) {
+  const initialConstraint = {
+    video: true,
+    audio: true,
+  };
+  const currentConstraint = {
+    video: { deviceId: cameraId ? { exact: cameraId } : undefined },
+    audio: { deviceId: audioId ? { exact: audioId } : undefined },
+  };
   try {
-    myMediaStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
     myDevices = await navigator.mediaDevices.enumerateDevices();
-    settingCameraList(myDevices);
-    setttingAudioList(myDevices);
-    myVideo.srcObject = myMediaStream;
+    myMediaStream = await navigator.mediaDevices.getUserMedia(
+      cameraId ? currentConstraint : initialConstraint
+    );
+    if (!cameraId) {
+      settingCameraList();
+      setttingAudioList();
+    }
+    myCameraVideo.srcObject = myMediaStream;
   } catch (e) {
     console.log(e);
   }
 }
 
-function handleMyCameraBt(event) {
-  event.preventDefault();
-  if (myCamera) {
-    myMediaStream.getVideoTracks()[0].stop();
-    myCamera = false;
-    myVideoOffBt.innerText = "Turn on Camera";
-  } else {
-    console.log(myMediaStream.getVideoTracks());
-    getMyCamera();
-    myCamera = true;
-    myVideoOffBt.innerText = "Turn off Camera";
-  }
-}
+// function handleMyCameraBt(event) {
+//   event.preventDefault();
+//   if (myCamera) {
+//     myMediaStream.getVideoTracks()[0].stop();
+//     myCamera = false;
+//     myVideoOffBt.innerText = "Turn on Camera";
+//   } else {
+//     getMyMedia();
+//     myCamera = true;
+//     myVideoOffBt.innerText = "Turn off Camera";
+//   }
+// }
 
-function handleMyAudioBt(event) {
-  event.preventDefault();
-  if (myAudio) {
-    myMediaStream.getAudioTracks()[0].stop();
-    myAudio = false;
-    myAudioMuteBt.innerText = "unMute";
-  } else {
-    getMyCamera();
-    myAudio = true;
-    myAudioMuteBt.innerText = "Mute";
-  }
-}
+// function handleMyAudioBt(event) {
+//   event.preventDefault();
+//   if (myAudio) {
+//     myMediaStream.getAudioTracks()[0].stop();
+//     myAudio = false;
+//     myAudioMuteBt.innerText = "unMute";
+//   } else {
+//     getMyMedia();
+//     myAudio = true;
+//     myAudioMuteBt.innerText = "Mute";
+//   }
+// }
 
-myVideoOffBt.addEventListener("click", handleMyCameraBt);
-myAudioMuteBt.addEventListener("click", handleMyAudioBt);
+// myVideoOffBt.addEventListener("click", handleMyCameraBt);
+// myAudioMuteBt.addEventListener("click", handleMyAudioBt);
 
-//landing page
-function handleNickname(event) {
-  event.preventDefault();
-  const input = nickNameForm.querySelector("input");
-  console.log(`submit Nick Name: ${input.value}`);
-  myNickname = input.value;
-  submitNickName.hidden = true;
-  submitedNickName.hidden = false;
-  const h3 = document.createElement("h3");
-  h3.innerText = `User Nickname : ${myNickname}`;
-  submitedNickName.appendChild(h3);
-  input.value = "";
-}
-function handleRoomname(event) {
-  event.preventDefault();
-  const input = roomNameForm.querySelector("input");
-  const roomList = roomListDiv.querySelector("h3");
-  console.log(`submit Room Name: ${input.value}`);
-  roomName = input.value;
-  const li = document.createElement("li");
-  li.innerText = roomName;
-  roomList.appendChild(li);
-  input.value = "";
-  landingPage.hidden = true;
-  callPage.hidden = false;
-  initCallPage();
-}
-function initCallPage() {
-  const roomNameh2 = document.createElement("h2");
-  roomNameh2.innerText = `RoomName : ${roomName}`;
-  callPage.prepend(roomNameh2);
-  const nickNameh2 = document.createElement("h2");
-  nickNameh2.innerText = `My NickName : ${myNickname}`;
-  callPage.prepend(nickNameh2);
-  getMyCamera();
-}
+// function handleAudioChange(event) {
+//   event.preventDefault();
+//   getMyMedia(myCameraSelect.value, myAudioSelect.value);
+// }
 
-nickNameForm.addEventListener("submit", handleNickname);
-roomNameForm.addEventListener("submit", handleRoomname);
+// function handleCameraChange(event) {
+//   event.preventDefault();
+//   getMyMedia(myCameraSelect.value, myAudioSelect.value);
+// }
+
+// myAudioSelect.addEventListener("change", handleAudioChange);
+// myCameraSelect.addEventListener("change", handleCameraChange);
